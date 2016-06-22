d90823a02b20c8a8b1c99ee06d4c5a9f9cca23ac
andaistvan <andaistvan@gmail.com>
2016-06-22 22:54:05 +0200

    kapcsolat & rolunk ready

--- themes/speed/parts/section6.php
+++ themes/speed/parts/section6.php
@@ -1,14 +1,30 @@
 <a id="section_six"></a>
-
+<!-- section6 - rólunk -->
    <div id="section6">
-      <div class="row">
-         <div class="medium-4 columns">
+      <div class="main-container">
+         <div id="rolunk-cont">
 
-         </div>
-         <div class="medium-4 columns">
-            <h2>section 6</h2>
-         </div>
-         <div class="medium-4 columns">
+            <div class="left-col">
+               <h3>Kik vagyunk?</h3>
+            </div><!-- left-col -->
+
+            <div class="right-col">
+               <p>
+                  Az ElmoStix Visual Poi története 2015-ben kezdődött. Ekkor döntöttünk úgy, hogy Magyarországon elsőként látványos műsorokat kezdünk építeni erre a világviszonylatban is újdonságnak számító eszközre.<br>
+                  Az ElmoStix Visual Poi bármilyen rendezvényen ideális vizuális kiegészítő lehet, hiszen a komplex üzenetek átadását mellett egy lenyűgöző showelemmel is gazdagítja az eseményt.
+                  Emellett hagyományos hulahopp- és zsonglőrszámokat is kínálunk ügyfeleinknek, profi csapatunk előadásában.
+               </p>
+               <p class="name">Ricsi</p>
+               <p>
+                  2000-ben kezdtem foglalkozni zsonglőrjátékok fejlesztésével, készítésével és értékesítésével. Az önálló ElmoStix márkát 2005-ben hoztam létre, ekkor léptem ki a nemzetközi piacra is. Az elmúlt 15 évben szinte nem volt olyan party, vásár vagy fesztivál, amin ne lettem volna ott. Jelenleg Magyarország egyik legismertebb zsonglőreszköz-készítője vagyok. Az ElmoStix pedig fogalom lett a profik körében.
+               </p>
+               <p class="name">
+                  Anna
+               </p>
+               <p>
+                  Magyarország első okleveles hulahopp-oktatójaként én alapítottam az első hazai hoopdance iskolát is. Az önálló számaim mellett artistákkal is gyakran lépek színpadra, zsonglőrködöm és egyedi hulahoppkarikákat is készítek. Az újcirkuszos performanszok mellett a LED-es hulahoppot is kipróbáltam, innen már egyenes út vezetett a Visual Poihoz.
+               </p>
+            </div><!-- right-col -->
 
          </div>
       </div>
--- themes/speed/parts/section7.php
+++ themes/speed/parts/section7.php
@@ -1,15 +1,111 @@
 <a id="section_seven"></a>
-
+<!-- section7 - kapcsolat -->
    <div id="section7">
-      <div class="row">
-         <div class="medium-4 columns">
-            <h2>section 7</h2>
-         </div>
-         <div class="medium-4 columns">
+      <div class="main-container">
+         <div id="kapcsolat-cont">
 
-         </div>
-         <div class="medium-4 columns">
+            <h3>Groszmann Rihárd</h3>
+            <p>+36 20 394 24 89<br>
+               <a href="mailto:richard.groszmann@elmostix.com">richard.groszmann@elmostix.com</a>
+            </p>
 
-         </div>
-      </div>
+            <h3>Márványi Anna</h3>
+            <p>+36 30 611 58 60<br>
+               <a href="mailto:anna.marvanyi@elmostix.com">anna.marvanyi@elmostix.com</a>
+            </p>
+
+         </div><!-- kapcsolat-cont -->
+
+         <h3>Lépjen velünk kapcsolatba!</h3>
+
+         <div id="kapcsolat-form-cont">
+
+            <div id="ninja_forms_form_5_cont" class="ninja-forms-cont">
+                <div id="ninja_forms_form_5_wrap" class="ninja-forms-form-wrap">
+                    <div id="ninja_forms_form_5_response_msg" style="" class="ninja-forms-response-msg "></div>
+                    <form id="ninja_forms_form_5" enctype="multipart/form-data" method="post" name="" action="" class="ninja-forms-form">
+
+                        <input type="hidden" id="_wpnonce" name="_wpnonce" value="f2e39930c7" />
+                        <input type="hidden" name="_wp_http_referer" value="/omnibus-werner/" />
+                        <input type="hidden" name="_ninja_forms_display_submit" value="1">
+                        <input type="hidden" name="_form_id" id="_form_id" value="5">
+                        <!-- <div class="hp-wrap">
+                           <label>If you are a human and are seeing this field, please leave it blank.
+                                <input type="text" value="" name="_wNRb2">
+                                <input type="hidden" value="_wNRb2" name="_hp_name">
+                            </label>
+                        </div> -->
+                        <div id="ninja_forms_form_5_all_fields_wrap" class="ninja-forms-all-fields-wrap">
+                            <div class="ninja-forms-required-items">A csillaggal jelölt mezők kitöltése kötelező!</div>
+
+
+               <div class="medium-6 columns">
+
+
+                     <!-- név -->
+                        <div class="field-wrap text-wrap label-inside" id="ninja_forms_field_5_div_wrap" data-visible="1">
+                            <input type="hidden" id="ninja_forms_field_5_type" value="text">
+                            <input id="ninja_forms_field_5" data-mask="" data-input-limit="" data-input-limit-type="char" data-input-limit-msg="character(s) left" name="ninja_forms_field_5" type="text" placeholder="" class="ninja-forms-field  ninja-forms-req " value="Név *" rel="5"
+                            />
+                            <input type="hidden" id="ninja_forms_field_5_label_hidden" value="Név *">
+                            <div id="ninja_forms_field_5_error" style="display:none;" class="ninja-forms-field-error">
+                            </div>
+                        </div>
+
+
+                     <!-- e-mail -->
+                        <div class="field-wrap text-wrap label-inside" id="ninja_forms_field_7_div_wrap" data-visible="1">
+                            <input type="hidden" id="ninja_forms_field_7_type" value="text">
+                            <input id="ninja_forms_field_7" data-mask="" data-input-limit="" data-input-limit-type="char" data-input-limit-msg="" name="ninja_forms_field_7" type="text" placeholder="" class="ninja-forms-field  ninja-forms-req email " value="E-mail *" rel="7" />
+                            <input type="hidden" id="ninja_forms_field_7_label_hidden" value="E-mail *">
+                            <div id="ninja_forms_field_7_error" style="display:none;" class="ninja-forms-field-error">
+                            </div>
+                        </div>
+
+
+                     <!-- telefon -->
+                        <div class="field-wrap text-wrap label-inside" id="ninja_forms_field_8_div_wrap" data-visible="1">
+                            <input type="hidden" id="ninja_forms_field_8_type" value="text">
+                            <input id="ninja_forms_field_8" data-mask="" data-input-limit="" data-input-limit-type="char" data-input-limit-msg="" name="ninja_forms_field_8" type="text" placeholder="" class="ninja-forms-field  " value="Telefon" rel="8" />
+                            <input type="hidden" id="ninja_forms_field_8_label_hidden" value="Telefon">
+                            <div id="ninja_forms_field_8_error" style="display:none;" class="ninja-forms-field-error">
+                            </div>
+                        </div>
+
+
+
+               </div><!-- medium-6 columns leftcol -->
+
+               <div class="medium-6 columns"><!-- medium-6 columns rightcol -->
+
+                  <div class="field-wrap textarea-wrap label-inside" id="ninja_forms_field_9_div_wrap" data-visible="1">
+                      <input type="hidden" id="ninja_forms_field_9_type" value="textarea">
+                      <textarea name="ninja_forms_field_9" id="ninja_forms_field_9" class="ninja-forms-field  ninja-forms-req" rel="9" data-input-limit="" data-input-limit-type="char" data-input-limit-msg="character(s) left">Üzenet *</textarea>
+                      <input type="hidden" id="ninja_forms_field_9_label_hidden" value="Üzenet *">
+                      <div id="ninja_forms_field_9_error" style="display:none;" class="ninja-forms-field-error">
+                      </div>
+                  </div>
+
+               </div><!-- medium-6 columns rightcol -->
+
+               <div class="small-12 columns">
+                  <!-- submit -->
+                  <div class="field-wrap submit-wrap label-above" id="ninja_forms_field_6_div_wrap" data-visible="1">
+                     <input type="hidden" id="ninja_forms_field_6_type" value="submit">
+                     <div id="nf_submit_5">
+                         <input type="submit" name="_ninja_forms_field_6" class="ninja-forms-field " id="ninja_forms_field_6" value="Elküld" rel="6">
+                     </div>
+                     <div id="nf_processing_5" style="display:none;">
+                         <input type="submit" name="_ninja_forms_field_6" class="ninja-forms-field " id="ninja_forms_field_6" value="Processing" rel="6" disabled>
+                     </div>
+                     <div id="ninja_forms_field_6_error" style="display:none;" class="ninja-forms-field-error">
+                     </div>
+                  </div>
+
+               </div><!-- small-12 columns -->
+
+         </div><!-- kapcsolat-form-cont -->
+
+      </div><!-- main-container -->
+
    </div>
